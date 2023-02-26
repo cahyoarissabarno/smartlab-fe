555117fa55ef92a2447f216918f0fec8b2e0482f
cahyo arissabarno <cahyoarissabarno@gmail.com>
2023-02-26 10:34:50 +0700

    update vite hmr config

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,11 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
+    https: false,
     hmr: {
-      host: "localhost",
+      host: "103.106.72.182",
       port: 5174,
       protocol: "wss",
-  },
+    },
   }
 })
